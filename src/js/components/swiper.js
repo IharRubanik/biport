af38f0bae647c9af1939152786fd441fdae78e55
Vitaly Reutsky <vreutsky77@mail.ru>
2022-08-25 23:24:00 +0300

    Сверстал и адаптировал под 768px страницу продукта

--- src/js/components/swiper.js
+++ src/js/components/swiper.js
@@ -1,4 +1,11 @@
-import Swiper, { EffectFade, Keyboard, Navigation, Pagination } from "swiper";
+import Swiper, {
+  EffectFade,
+  Keyboard,
+  Navigation,
+  Pagination,
+  Scrollbar,
+  Thumbs,
+} from "swiper";
 
 //hero-swiper
 Swiper.use([Navigation, Pagination, EffectFade]);
@@ -127,6 +134,92 @@ const servicesSwiper = new Swiper(".services__swiper", {
     },
     1200: {
       slidesPerView: 2.65,
+    },
+  },
+});
+
+//door-swiper
+Swiper.use([Navigation, Pagination, Keyboard, Thumbs, Scrollbar]);
+const doorSwiper = new Swiper(".door__swiper", {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 20,
+
+  speed: 1000,
+
+  scrollbar: {
+    el: ".door__scrollbar",
+    draggable: true,
+  },
+
+  thumbs: {
+    swiper: {
+      el: ".door__swiper-mini",
+      slidesPerView: 3,
+      spaceBetween: 10,
+      breakpoints: {
+        0: {
+          direction: "horizontal",
+          slidesPerView: 2,
+        },
+        600: {
+          direction: "vertical",
+          slidesPerView: 3,
+        },
+        1200: {
+          direction: "horizontal",
+        },
+      },
+    },
+  },
+
+  breakpoints: {
+    0: {
+      direction: "horizontal",
+    },
+    600: {
+      direction: "vertical",
+    },
+    1200: {
+      direction: "horizontal",
+    },
+  },
+});
+
+//recent-swiper
+Swiper.use([Navigation, Pagination, Keyboard]);
+const recentSwiper = new Swiper(".recent__swiper", {
+  spaceBetween: 20,
+  loop: false,
+  autoHeight: true,
+  centeredSlides: false,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+
+  speed: 1000,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      slidesPerGroup: 1,
+    },
+    600: {
+      slidesPerView: 1.9,
+      slidesPerGroup: 1,
+      centeredSlides: true,
+      loop: true,
+    },
+    1200: {
+      slidesPerView: 2.85,
+      slidesPerGroup: 2,
+      centeredSlides: false,
+      loop: false,
     },
   },
 });
--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -1,5 +1,7 @@
 import GraphTabs from "graph-tabs";
 const graphTabs = document.querySelector("[data-tabs]");
+const tabsSize = document.querySelector("[data-size]");
+
 if (graphTabs) {
   const tabs = new GraphTabs("catalog");
 
@@ -15,6 +17,23 @@ if (graphTabs) {
 
     if (target.classList.contains("active-btn")) {
       target.classList.add("tabs__nav-btn--active");
+    }
+  });
+}
+
+if (tabsSize) {
+  const sizeNav = document.querySelector(".size__nav");
+
+  sizeNav.addEventListener("click", function (e) {
+    const items = document.querySelectorAll(".size__btn-active");
+    const target = e.target;
+
+    Array.from(items).forEach((item) => {
+      item.classList.remove("size__nav-btn--active");
+    });
+
+    if (target.classList.contains("size__btn-active")) {
+      target.classList.add("size__nav-btn--active");
     }
   });
 }
